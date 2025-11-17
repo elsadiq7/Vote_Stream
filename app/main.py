bebd093bda56d6a455708acbc8d4eaebd0d3ffcb
Abdulrahman Elsadiq <abdelrhmanelsadiq53@gmail.com>
2025-11-17 15:20:23 +0200

    final

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from ssl import create_default_context  # For secure SSL connections (not used h
 from turtle import mode, title
 from fastapi import FastAPI
 from random import randrange
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.outh2 import SECRET_KEY
 
@@ -22,7 +23,15 @@ settings=Settings()
 # ===========================================================
 app = FastAPI(title="FastAPI Posts API", version="1.0")
 
-
+origins=[
+    "*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
